92ce6f60ed7af8a9c58cbfbfc550edacf000e3cb
crushed-lemon <crushed.lemon.official@gmail.com>
2025-10-07 00:07:23 +0530

    Change GET Apis so that they read from request params rather than request body

--- src/main/java/com/crushedlemon/chess/controllers/OngoingStateController.java
+++ src/main/java/com/crushedlemon/chess/controllers/OngoingStateController.java
@@ -8,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -23,39 +22,33 @@ public class OngoingStateController {
 
     @GetMapping("/ongoing-lobby")
     ResponseEntity<Boolean> onGoingLobbyRequests(
-            @RequestBody Map<String, Object> payload) {
-        String userName = (String) payload.get("userName");
-
-        Optional<GamePreferences> gamePreferences = chessService.getOngoingLobbyRequest(userName);
+            @RequestParam(name = "username") String username) {
+        Optional<GamePreferences> gamePreferences = chessService.getOngoingLobbyRequest(username);
 
         return ResponseEntity.ok(gamePreferences.isPresent());
     }
 
     @GetMapping("/ongoing-game-id")
     ResponseEntity<String> onGoingGameId(
-            @RequestBody Map<String, Object> payload) {
-        String userName = (String) payload.get("userName");
-
-        Optional<String> gameId = chessService.getOngoingGameId(userName);
+            @RequestParam(name = "username") String username) {
+        Optional<String> gameId = chessService.getOngoingGameId(username);
 
         return gameId.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.ok(null));
     }
 
     @GetMapping("/ongoing-game")
     public ResponseEntity<String> getGame(
-            @RequestBody Map<String, Object> payload) {
-        String userName = (String) payload.get("userName");
-        String gameId = (String) payload.get("gameId");
-
+            @RequestParam(name = "username") String username,
+            @RequestParam(name = "gameId") String gameId) {
         Optional<Game> game = chessService.getOngoingGame(gameId);
 
         if(game.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Game id %s not found", gameId));
         }
 
-        if(!game.get().getWhitePlayerId().equals(userName) && !game.get().getBlackPlayerId().equals(userName)) {
+        if(!game.get().getWhitePlayerId().equals(username) && !game.get().getBlackPlayerId().equals(username)) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    String.format("%s is not allowed to access game id : %s", userName, gameId));
+                    String.format("%s is not allowed to access game id : %s", username, gameId));
         }
 
         return ResponseEntity.ok(game.get().getBoard().getPieces());
